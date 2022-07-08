82640c2576f4f02325200654b5e46e75f961f443
Sardor0795 <english.programmer.9595@gmail.com>
2022-07-08 17:13:05 +0500

    Add object to js file

--- js/main.js
+++ js/main.js
@@ -8,85 +8,94 @@ const elEasy = document.querySelector(".easy");
 const elsAccordionWrapper = document.querySelectorAll(".accordion__wrapper")
 const elsAccordionBtn = document.querySelectorAll(".accordion__btn");
 
+
+const opener = {
+  featureBtn: "features__btn--active",
+  elHide: "el-hider",
+  elShow: "el-shower",
+  accordionOpen: "accordion__wrapper--open",
+}
+
+
 elBtnWrapper.addEventListener("click", function (evt) {
 
   if ( evt.target.classList.contains("features__btn")) {
 
     if( evt.target.previousElementSibling ){
 
-        if ( evt.target.previousElementSibling.classList.contains("features__btn--active") ) {
-          evt.target.previousElementSibling.classList.remove("features__btn--active");
+        if ( evt.target.previousElementSibling.classList.contains(opener.featureBtn) ) {
+          evt.target.previousElementSibling.classList.remove(opener.featureBtn);
         }
 
         if( evt.target.previousElementSibling.previousElementSibling ) {
-            if ( evt.target.previousElementSibling.previousElementSibling.classList.contains("features__btn--active")
+            if ( evt.target.previousElementSibling.previousElementSibling.classList.contains(opener.featureBtn)
             ) {
-              evt.target.previousElementSibling.previousElementSibling.classList.remove("features__btn--active");
+              evt.target.previousElementSibling.previousElementSibling.classList.remove(opener.featureBtn);
             }
         }
     }
 
     if( evt.target.nextElementSibling ){
 
-        if ( evt.target.nextElementSibling.classList.contains("features__btn--active") ) {
-          evt.target.nextElementSibling.classList.remove("features__btn--active");
+        if ( evt.target.nextElementSibling.classList.contains(opener.featureBtn) ) {
+          evt.target.nextElementSibling.classList.remove(opener.featureBtn);
         }
 
         if( evt.target.nextElementSibling.nextElementSibling ) {
-            if ( evt.target.nextElementSibling.nextElementSibling.classList.contains("features__btn--active")
+            if ( evt.target.nextElementSibling.nextElementSibling.classList.contains(opener.featureBtn)
             ) {
-              evt.target.nextElementSibling.nextElementSibling.classList.remove("features__btn--active");
+              evt.target.nextElementSibling.nextElementSibling.classList.remove(opener.featureBtn);
             }
         }
     }
 
-    evt.target.classList.add("features__btn--active");
+    evt.target.classList.add(opener.featureBtn);
 
-    
+
     if ( evt.target.classList.contains("easy-btn") ) {
 
-        elSimple.classList.add("el-hider")
-        elSimple.classList.remove("el-shower")
+        elSimple.classList.add(opener.elHide)
+        elSimple.classList.remove(opener.elShow)
 
-        elSpeedy.classList.add("el-hider")
-        elSpeedy.classList.remove("el-shower")
-        
-        elEasy.classList.remove("el-hider");
-        elEasy.classList.add("el-shower");
+        elSpeedy.classList.add(opener.elHide)
+        elSpeedy.classList.remove(opener.elShow)
+
+        elEasy.classList.remove(opener.elHide);
+        elEasy.classList.add(opener.elShow);
 
     }  else if ( evt.target.classList.contains("speedy-btn") ) {
 
-        elSimple.classList.add("el-hider")
-        elSimple.classList.remove("el-shower")
+        elSimple.classList.add(opener.elHide)
+        elSimple.classList.remove(opener.elShow)
 
-        elEasy.classList.add("el-hider")
-        elEasy.classList.remove("el-shower")
-        
-        elSpeedy.classList.remove("el-hider");
-        elSpeedy.classList.add("el-shower");
+        elEasy.classList.add(opener.elHide)
+        elEasy.classList.remove(opener.elShow)
+
+        elSpeedy.classList.remove(opener.elHide);
+        elSpeedy.classList.add(opener.elShow);
 
     } else if ( evt.target.classList.contains("simple-btn") ) {
 
-        elSpeedy.classList.add("el-hider")
-        elSpeedy.classList.remove("el-shower")
+        elSpeedy.classList.add(opener.elHide)
+        elSpeedy.classList.remove(opener.elShow)
 
-        elEasy.classList.add("el-hider")
-        elEasy.classList.remove("el-shower")
-        
-        elSimple.classList.remove("el-hider");
-        elSimple.classList.add("el-shower");
+        elEasy.classList.add(opener.elHide)
+        elEasy.classList.remove(opener.elShow)
+
+        elSimple.classList.remove(opener.elHide);
+        elSimple.classList.add(opener.elShow);
     }
-  
+
   }
 });
 
 elsAccordionBtn.forEach(function (elAccordionBtn) {
   elAccordionBtn.addEventListener("click", function () {
-    
+
     elsAccordionWrapper.forEach(function (elAccordionWrapper) {
-      elAccordionWrapper.classList.remove("accordion__wrapper--open")
+      elAccordionWrapper.classList.remove(opener.accordionOpen)
     })
 
-    elAccordionBtn.closest(".accordion__wrapper").classList.add("accordion__wrapper--open")
+    elAccordionBtn.closest(".accordion__wrapper").classList.add(opener.accordionOpen)
   })
 })
